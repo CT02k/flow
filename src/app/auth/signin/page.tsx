333208d8923cb9b5bc7707ae45e405516f476b6b
CT02k <joaopaulocfwork@gmail.com>
2025-10-22 20:14:52 -0300

    finished login page and added branding assets

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,27 +1,99 @@
 "use client";
 
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signIn } from "next-auth/react";
 import { useTranslations } from "next-intl";
 import { useRouter } from "next/navigation";
 
+import Image from "next/image";
+import { siDiscord, siGithub, SimpleIcon } from "simple-icons";
+import { useState } from "react";
+import { Loader2 } from "lucide-react";
+
+export interface Provider {
+  icon?: SimpleIcon;
+  name: string;
+  value: string;
+  colors: string[];
+}
+
+const providers: Provider[] = [
+  {
+    icon: siDiscord,
+    name: "Discord",
+    value: "discord",
+    colors: [
+      "bg-[#5865f2]",
+      "text-white",
+      "hover:not-disabled:bg-[#5865f2]/90",
+      "fill-white",
+    ],
+  },
+  {
+    icon: siGithub,
+    name: "GitHub",
+    value: "github",
+    colors: [
+      "bg-black",
+      "text-white",
+      "hover:not-disabled:bg-black/90",
+      "fill-white",
+    ],
+  },
+];
+
 export default function Component() {
   const t = useTranslations("SignIn");
-  const { data: session } = useSession();
   const router = useRouter();
+  const { data: session } = useSession();
+  const [loading, setLoading] = useState({ state: false, provider: "" });
 
   //   if (session) {
   //     return router.push("/dash");
   //   }
 
   return (
-    <div className="flex w-full h-screen items-center justify-center bg-zinc-2001">
-      <div className="providers flex flex-col gap-3">
-        <button>
-          {t("signInWith", {
-            provider: "sexo",
-          })}
-        </button>
+    <div className="flex flex-row gap-5 w-full h-screen items-center justify-start bg-zinc-200">
+      <div className="w-1/2 flex flex-col items-center">
+        <Image
+          src="/branding/logo_full_default_black.svg"
+          alt="Full Logo"
+          width={200}
+          height={100}
+        />
+        <div className="providers flex flex-col mt-10 gap-3 w-72 h-40">
+          {providers.map((p) => (
+            <button
+              key={p.value}
+              onClick={() => {
+                setLoading({
+                  state: true,
+                  provider: p.value,
+                });
+                signIn(p.value);
+              }}
+              className={`${p.colors.join(" ")} flex items-center justify-center py-2.5 rounded-lg cursor-pointer transition disabled:cursor-auto disabled:opacity-50`}
+              disabled={loading.state}
+            >
+              {loading.state && loading.provider === p.value ? (
+                <Loader2 className="animate-spin" />
+              ) : (
+                <>
+                  {p.icon && (
+                    <span
+                      className="inline-block w-5 h-5 mr-2"
+                      dangerouslySetInnerHTML={{ __html: p.icon.svg }}
+                    />
+                  )}
+                  {t("signInWith", {
+                    provider: p.name,
+                  })}
+                </>
+              )}
+            </button>
+          ))}
+        </div>
       </div>
+      <div className="bg-linear-to-b from-[#00EA8D] to-[#2A005E] h-full w-1/2 flex flex-col items-center justify-center"></div>
     </div>
   );
 }
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,17 +2,36 @@
 
 import { detectLocale } from "@/lib/i18n";
 import { SessionProvider } from "next-auth/react";
-import { NextIntlClientProvider } from "next-intl";
-
-import { ReactNode, use } from "react";
+import { Locale, NextIntlClientProvider } from "next-intl";
+import { ReactNode, useEffect, useState } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
-  const locale = use(detectLocale());
+  const [locale, setLocale] = useState<Locale>("en");
+  const [messages, setMessages] = useState();
 
-  const messages = use(import(`@/messages/${locale}.json`)).default;
+  useEffect(() => {
+    async function loadMessages() {
+      const lcl = await detectLocale();
+      const msg = (await import(`@/messages/${lcl}.json`)).default;
+
+      setLocale(lcl);
+      setMessages(msg);
+    }
+
+    loadMessages();
+  }, []);
+
+  if (!messages) {
+    return null;
+  }
+
   return (
     <SessionProvider>
-      <NextIntlClientProvider locale={locale} messages={messages}>
+      <NextIntlClientProvider
+        locale={locale}
+        timeZone="America/Sao_Paulo"
+        messages={messages}
+      >
         {children}
       </NextIntlClientProvider>
     </SessionProvider>
